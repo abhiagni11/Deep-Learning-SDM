9e64abcf1ced97dba7d3fbf0e8abe6295734e701
abhiagni11 <abhijeet.agni.1993@gmail.com>
2019-02-12 11:03:29 -0800

    uncomment

--- underground.py
+++ underground.py
@@ -18,7 +18,6 @@ class Underground:
 		self._y_dim, self._x_dim = self._tunnel_map.shape
 		self._action_dict = {"none": 0, "up": 1, "right": 2, "down": 3, "left": 4}
 		self._action_coords = [(0, 0), (0, -1), (1, 0), (0, 1), (-1, 0)]
-		print(self._tunnel_map)
 
 	def _check_state_in_tunnel(self, state):
 		return self._tunnel_map[state[1]][state[0]]
@@ -29,8 +28,8 @@ class Underground:
 			new_state = [state[0] + self._action_coords[value][0], state[1] + self._action_coords[value][1]]
 			if self._check_state_in_tunnel(new_state):
 				allowed_actions.append(key)
-				print('action: {} and new_state: {} and key: {}'.format(self._check_state_in_tunnel(new_state), new_state, key))
-		print('allowed action: {} and state {}'.format(allowed_actions, state))
+				# print('action: {} and new_state: {} and key: {}'.format(self._check_state_in_tunnel(new_state), new_state, key))
+		# print('allowed action: {} and state {}'.format(allowed_actions, state))
 		return allowed_actions
 
 	def _found_artifact(self, state):
