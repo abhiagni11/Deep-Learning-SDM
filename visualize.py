a028015968c5a65094c0feb4a42fdc6f4d5d0a02
abhiagni11 <abhijeet.agni.1993@gmail.com>
2019-02-19 03:28:16 -0800

    visualize observation space and remaining artifacts

--- visualize.py
+++ visualize.py
@@ -14,24 +14,42 @@ import matplotlib.patches as patches
 
 class Visualize:
 
-	def __init__(self, tunnel_filename, artifact_filename):
+	def __init__(self, tunnel_filename):
 		self._tunnel_map = np.load(tunnel_filename)
-		self._artifact_locations = [(x[1], x[0]) for x in np.load(artifact_filename).tolist()]
 		self._x_dim, self._y_dim = self._tunnel_map.shape
 		self.fig, self.ax = plt.subplots()
 
-	def _initialise_visualization(self):
+	def _initialise_visualization(self, artifact_locations):
+		self._artifact_locations = artifact_locations
 		plt.tight_layout()
 		plt.imshow(self._tunnel_map, cmap=plt.get_cmap('bone'))
 		plt.ion()
 		plt.show()
 
-	def _keep_visualizing(self, robot_states):
+	def _check_state_in_tunnel(self, state):
+		# state = (x, y)
+		if state[0] < 0 or state[1] < 0 or state[0] > self._x_dim or state[1] > self._y_dim:
+			return 0
+		else:
+			return self._tunnel_map[state[1]][state[0]]
+
+	def _keep_visualizing(self, robot_states, updated_artifact_locations, current_observation):
 		# TODO: Adapt for multiple robots
+		self._artifact_locations = updated_artifact_locations
 		plt.cla()
 		plt.imshow(self._tunnel_map, cmap=plt.get_cmap('bone'))
+		observation_radius = len(current_observation[0])//2
+
+		for y in range(observation_radius*2 + 1):
+			for x in range(observation_radius*2 + 1):
+				_i_state = (robot_states[0] + x - observation_radius, robot_states[1] + y - observation_radius)
+				if self._check_state_in_tunnel(_i_state):
+					rect = patches.Rectangle((_i_state[0] - 0.5, _i_state[1] - 0.5), 1, 1, linewidth=1, edgecolor='g', facecolor='g')
+					self.ax.add_patch(rect)
+
 		rect = patches.Rectangle((robot_states[0] - 0.5, robot_states[1] - 0.5), 1, 1, linewidth=1, edgecolor='r', facecolor='r')
 		self.ax.add_patch(rect)
+		
 		for artifact in self._artifact_locations:
 			rect = patches.Rectangle((artifact[0] - 0.5, artifact[1] - 0.5), 1, 1, linewidth=1, edgecolor='b', facecolor='none')
 			self.ax.add_patch(rect)
@@ -39,3 +57,16 @@ class Visualize:
 		# plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
 		plt.draw()
 		plt.pause(.1)
+
+
+	def _get_observation(self, state):
+		# state is (x, y)
+		self._current_observation = np.zeros((self._observation_radius*2 + 1, self._observation_radius*2 + 1))
+		for y in range(self._observation_radius*2 + 1):
+			for x in range(self._observation_radius*2 + 1):
+				_i_state = (state[0] + x - self._observation_radius, state[1] + y - self._observation_radius)
+				if self._check_state_in_tunnel(_i_state):
+					self._current_observation[y][x] = 1
+				else:
+					self._current_observation[y][x] = 0
+		return self._current_observation
