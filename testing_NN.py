430181fa654907e6fa85e1d35f65d48780b7bad1
abhiagni11 <abhijeet.agni.1993@gmail.com>
2019-03-06 12:37:23 -0800

    adding more images

--- testing_NN.py
+++ testing_NN.py
@@ -65,10 +65,11 @@ def main(value_dist, TUNNEL_FILE, ARTIFACT_FILE, visualize=True):
 	observation = tunnel._get_observation(state)
 	wall_e.update_observed_map(observation, tunnel._observation_radius)
 
+	# rest_halt = input("enter input")
+	
 	# Runs until Wall-e runs out of budget or there are no frontiers left to explore
 	try:
 		while steps < budget and len(tunnel._updated_artifact_locations) > 0:
-			# rest_halt = input("enter input")
 			# Get matrix of observed frontier values around wall-e and update observed map
 			observation = tunnel._get_observation(state)
 			wall_e.update_observed_map(observation, tunnel._observation_radius)
@@ -124,8 +125,9 @@ def main(value_dist, TUNNEL_FILE, ARTIFACT_FILE, visualize=True):
 
 if __name__ == "__main__":
 
-	tunnel_num = 44	
-	value_dist = 'sqrt'
+	tunnel_num = 30
+	# available value functions: 'value', 'quarter', 'closest', 'sqrt', 'normal'
+	value_dist = 'closest'
 	TUNNEL_FILE = './maps_{}/tunnel_{}.npy'.format(GRID_SIZE, tunnel_num)
 	ARTIFACT_FILE = './maps_{}/artifacts_{}.npy'.format(GRID_SIZE, tunnel_num)
 
