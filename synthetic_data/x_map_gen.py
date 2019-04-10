748f36ed77502ce46ae4274b957295836d959805
Manish <manishsaroya@gmail.com>
2019-04-10 03:47:33 -0700

    created exploration class

--- synthetic_data/x_map_gen.py
+++ synthetic_data/x_map_gen.py
@@ -8,17 +8,35 @@ Created on Mon Apr  8 22:32:42 2019
 
 from flood_fill import getTiles
 
+class Exploration:
+	"""Filters X% exploration map form completely known map 
+	   with appropriate flood fill based masking"""
+	      
+	def __init__(self, gridSize, numPOI, percentFilter):
+		self.gridSize = gridSize
+		self.gridDimension = [self.gridSize, self.gridSize]
+		self.numPOI = numPOI
+		self.percentFilter = percentFilter
+		self.occupancy_map = None
+	
+	def generate_map(self):
+		feature_map, self.occupancy_map = getTiles(self.gridDimension, self.numPOI)
+		print(self.occupancy_map)
+	
+	def nbrs(self, point):
+		pass
+	
+	def flood_fill_filter(self):
+		# adapted from map generation procedure
+		startp = [0, int(self.gridDimension[1]/2)]
+		latentPoints = []
+		latentPoints.append(startp)
+		while(len(latentPoints)>0):
+			latent = latentPoints.pop(0)
+			
+		
 GRID_SIZE = 16
-gridDimension = [GRID_SIZE, GRID_SIZE]
 numPOI = 9
-feature_map, occupancy_map = getTiles(gridDimension,numPOI)
-print(occupancy_map)
 
-# adapted from map generation procedure
-start_point = [0, int(gridDimension[1]/2)]
-
-latent_points = []
-latent_points.append(start_point)
-
-while(len(start_point)>0){
-        latent_points[0]}
+explore = Exploration(GRID_SIZE, numPOI,0)
+explore.generate_map()
